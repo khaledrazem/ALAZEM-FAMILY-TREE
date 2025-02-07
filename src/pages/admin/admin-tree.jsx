e343b9c345fab436424a91b2641e32d649535770
khaled alazem <khaledrazem1@gmail.com>
2025-02-07 23:17:31 +0000

    add data

--- src/pages/admin/admin-tree.jsx
+++ src/pages/admin/admin-tree.jsx
@@ -1,70 +1,89 @@
+import React, { useEffect, useRef } from "react";
+import f3 from '../../custome-modules/family-chart/dist/family-chart.js';
 
-import React from "react";
-import f3 from 'family-chart';  // npm install family-chart@0.2.1 or yarn add family-chart@0.2.1
-import 'family-chart/styles/family-chart.css';
+export default function FamilyTree() {
+  const containerRef = useRef();
+  const chartRef = useRef();
+  const editTreeRef = useRef();
 
-export default class FamilyTree extends React.Component {
-  cont = React.createRef();
+  useEffect(() => {
+    // Cleanup function to destroy previous chart instance
+    const cleanup = () => {
+      if (chartRef.current && containerRef.current) {
+        // Remove all event listeners and DOM elements
+        containerRef.current.innerHTML = '';
+        chartRef.current = null;
+        editTreeRef.current = null;
+      }
+    };
 
-  componentDidMount() {
-    if (!this.cont.current) return;
-    
-    create(data())
+    // Initialize chart
+    const initializeChart = () => {
+      if (!containerRef.current) return;
 
-    function create(data) {
-      const f3Chart = f3.createChart('#FamilyChart', data)
-        .setTransitionTime(1000)
-        .setCardXSpacing(250)
-        .setCardYSpacing(150)
-        .setOrientationVertical()
-        .setSingleParentEmptyCard(true, {label: 'ADD'})
-    
-      const f3Card = f3Chart.setCard(f3.CardHtml)
-        .setCardDisplay([["first name","last name"],["birthday"]])
-        .setCardDim({})
-        .setMiniTree(true)
-        .setStyle('imageRect')
-        .setOnHoverPathToMain()
-    
+      cleanup(); // Clean up any existing chart
+
+      function create(data) {
+        const f3Chart = f3.createChart('#FamilyChart', data)
+          .setTransitionTime(1000)
+          .setCardXSpacing(450)
+          .setCardYSpacing(450)
+          .setOrientationVertical()
+          .setSingleParentEmptyCard(true, {label: 'ADD'});
       
-      const f3EditTree = f3Chart.editTree()
-        .fixed(true)
-        .setFields(["first name","last name","birthday","avatar"])
-        .setEditFirst(true)
+        const f3Card = f3Chart.setCard(f3.CardHtml)
+          .setCardDisplay([["first name","last name"],["birthday"]])
+          .setCardDim({})
+          .setMiniTree(true)
+          .setStyle('custom')
+          .setOnHoverPathToMain();
       
-      f3EditTree.setEdit()
+        const f3EditTree = f3Chart.editTree()
+          .fixed(true)
+          .setFields(["first name","last name","birthday","avatar"])
+          .setEditFirst(true);
+        
+        f3EditTree.setEdit();
+        
+        f3Card.setOnCardClick((e, d) => {
+          f3EditTree.open(d);
+          if (f3EditTree.isAddingRelative()) return;
+          f3Card.onCardClickDefault(e, d);
+        });
       
-      f3Card.setOnCardClick((e, d) => {
-        f3EditTree.open(d)
-        if (f3EditTree.isAddingRelative()) return
-        f3Card.onCardClickDefault(e, d)
-      })
-    
-      f3Chart.updateTree({initial: true})
-      f3EditTree.open(f3Chart.getMainDatum())
-    
-      f3Chart.updateTree({initial: true})
-    }
-    
-    function data() {
-      return [
-        {
-          "id": "0",
-          "rels": {},
-          "data": {
-            "first name": "Name",
-            "last name": "Surname",
-            "birthday": 1970,
-            "avatar": "https://static8.depositphotos.com/1009634/988/v/950/depositphotos_9883921-stock-illustration-no-user-profile-picture.jpg",
-            "gender": "M"
+        f3Chart.updateTree({initial: true});
+        f3EditTree.open(f3Chart.getMainDatum());
+      
+        // Store references for cleanup
+        chartRef.current = f3Chart;
+        editTreeRef.current = f3EditTree;
+      }
+      
+      function data() {
+        return [
+          {
+            "id": "0",
+            "rels": {},
+            "data": {
+              "first name": "Name",
+              "last name": "Surname",
+              "birthday": 1970,
+              "avatar": "https://static8.depositphotos.com/1009634/988/v/950/depositphotos_9883921-stock-illustration-no-user-profile-picture.jpg",
+              "gender": "M"
+            }
           }
-        }
-      ]
-    }
+        ];
+      }
 
-  }
+      create(data());
+    };
 
-  render() {
-    return <div className="f3 f3-cont" id="FamilyChart" ref={this.cont}></div>;
-  }
+    // Initialize chart when component mounts
+    initializeChart();
+
+    // Cleanup on unmount
+    return cleanup;
+  }, []); // Empty dependency array since we don't need to re-run on any dependencies
+
+  return <div className="f3 f3-cont" id="FamilyChart" ref={containerRef}></div>;
 }
--- src/pages/tree.jsx
+++ src/pages/tree.jsx
@@ -1,87 +1,249 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import f3 from '../custome-modules/family-chart/dist/family-chart.js';
 import { useRouter } from 'next/router'
 
-export default class FamilyTree extends React.Component {
-  cont = React.createRef();
+export default function FamilyTree() {
+  const containerRef = useRef();
+  const chartRef = useRef();
+  const router = useRouter();
 
-  componentDidMount() {
-    if (!this.cont.current) return;
-    const { router } = this.props;
-
-    function toUserPage(id)   {
-      console.log('id')
-      console.log(id)
-      console.log(router)
-      if (!id) return;
-      router.push("/user/profile/" + id);
+  useEffect(() => {
+    // Cleanup function to destroy previous chart instance
+    const cleanup = () => {
+      if (chartRef.current && containerRef.current) {
+        // Remove all event listeners and DOM elements
+        containerRef.current.innerHTML = '';
+        chartRef.current = null;
+      }
     };
 
-    function create(data) {
-      const f3Chart = f3.createChart('#FamilyChart', data)
-        .setTransitionTime(1000)
-        .setCardXSpacing(450)
-        .setCardYSpacing(150)
-        .setOrientationVertical()
-        .setSingleParentEmptyCard(true, {label: 'ADD'})
-    
-      const f3Card = f3Chart.setCard(f3.CardHtml)
-        .setCardDisplay([["first name","last name"],["birthday"]])
-        .setCardDim({})
-        .setMiniTree(true)
-        .setStyle('custom')
-        .setOnHoverPathToMain()
-    
-      f3Card.setOnCardClick((e, d) => {
-        console.log("clicked")
-        console.log(d)
-        console.log(e)
-        toUserPage(d.data.id);
-      })
-    
-      f3Chart.updateTree({initial: true})
-    }
-    
-    function data() {
-      return [
-        {
-          "id": "0",
-          "rels": {
-            "spouses": [
-              "055a439f-d985-4128-aca8-24a2f0b9af7e"
-            ]
-          },
-          "data": {
-            "first name": "Name",
-            "last name": "Surname",
-            "birthday": 1970,
-            "avatar": "https://static8.depositphotos.com/1009634/988/v/950/depositphotos_9883921-stock-illustration-no-user-profile-picture.jpg",
-            "gender": "M"
-          }
-        },
-        {
-          "id": "055a439f-d985-4128-aca8-24a2f0b9af7e",
-          "data": {
-            "gender": "F",
-            "first name": "surname ",
-            "last name": "name",
-            "birthday": "02/05/2000",
-            "avatar": ""
-          },
-          "rels": {
-            "spouses": [
-              "0"
-            ],
-            "children": []
-          }
-        }
-      ]
-    }
+    // Initialize chart
+    const initializeChart = () => {
+      if (!containerRef.current) return;
 
-    create(data())
-  }
+      cleanup(); // Clean up any existing chart
 
-  render() {
-    return <div className="f3 f3-cont" id="FamilyChart" ref={this.cont}></div>;
-  }
+      function toUserPage(id) {
+        if (!id) return;
+        router.push("/user/profile/" + id);
+      }
+
+      function create(data) {
+        const f3Chart = f3.createChart('#FamilyChart', data)
+          .setTransitionTime(1000)
+          .setCardXSpacing(450)
+          .setCardYSpacing(450)
+          .setOrientationVertical()
+      
+        const f3Card = f3Chart.setCard(f3.CardHtml)
+          .setCardDisplay([["first name","last name"],["birthday"]])
+          .setCardDim({})
+          .setStyle('custom')
+          .setOnHoverPathToMain();
+      
+        f3Card.setOnCardClick((e, d) => {
+          toUserPage(d.data.id);
+        });
+      
+        f3Chart.updateTree({initial: true});
+        
+        // Store chart reference for cleanup
+        chartRef.current = f3Chart;
+      }
+      
+ function data() {
+          return [
+              {
+                  "id": "P1",
+                  "data": {
+                      "first name": "Reyad",
+                      "last name": "Alazem",
+                      "desc": "Father of the Alazem family",
+                      "label": "Reyad Alazem",
+                      "gender": "M",
+                      "birthYear": 1971
+                  },
+                  "rels": {
+                      "father": "P3",
+                      "mother": "P4",
+                      "spouses": ["P2"],
+                      "children": ["C1", "C2", "C3", "C4", "C5"]
+                  }
+              },
+              {
+                  "id": "P2",
+                  "data": {
+                      "first name": "Salma",
+                      "last name": "Alazem",
+                      "desc": "Mother of the Alazem family",
+                      "label": "Salma Alazem",
+                      "gender": "F",
+                      "birthYear": 1976
+                  },
+                  "rels": {
+                      "father": "P5",
+                      "mother": "P6",
+                      "spouses": ["P1"],
+                      "children": ["C1", "C2", "C3", "C4", "C5"]
+                  }
+              },
+             
+              {
+                  "id": "C1",
+                  "data": {
+                      "first name": "Khaled",
+                      "last name": "Alazem",
+                      "desc": "Eldest son",
+                      "label": "Khaled Alazem",
+                      "gender": "M",
+                      "birthYear": 2000
+                  },
+                  "rels": {
+                      "father": "P1",
+                      "mother": "P2"
+                  }
+              },
+              {
+                  "id": "C2",
+                  "data": {
+                      "first name": "Fatima",
+                      "last name": "Alazem",
+                      "desc": "Daughter",
+                      "label": "Fatima Alazem",
+                      "gender": "F",
+                      "birthYear": 2005
+                  },
+                  "rels": {
+                      "father": "P1",
+                      "mother": "P2"
+                  }
+              },
+              {
+                  "id": "C3",
+                  "data": {
+                      "first name": "Rama",
+                      "last name": "Alazem",
+                      "desc": "Daughter",
+                      "label": "Rama Alazem",
+                      "gender": "F",
+                      "birthYear": 2007
+                  },
+                  "rels": {
+                      "father": "P1",
+                      "mother": "P2"
+                  }
+              },
+              {
+                  "id": "C4",
+                  "data": {
+                      "first name": "Leen",
+                      "last name": "Alazem",
+                      "desc": "Daughter",
+                      "label": "Leen Alazem",
+                      "gender": "F",
+                      "birthYear": 2011
+                  },
+                  "rels": {
+                      "father": "P1",
+                      "mother": "P2"
+                  }
+              },
+              {
+                  "id": "C5",
+                  "data": {
+                      "first name": "Abdularahman",
+                      "last name": "Alazem",
+                      "desc": "Youngest son",
+                      "label": "Abdularahman Alazem",
+                      "gender": "M",
+                      "birthYear": 2015
+                  },
+                  "rels": {
+                      "father": "P1",
+                      "mother": "P2"
+                  }
+              },
+              {
+                  "id": "P3",
+                  "data": {
+                      "first name": "Khaled",
+                      "last name": "Alazem",
+                      "desc": "Father of Reyad Alazazem",
+                      "label": "Khaled Alazem",
+                      "gender": "M",
+                      "birthYear": 1953
+                  },
+                  "rels": {
+                      "spouses": ["P4"],
+                      "children": ["P1"]
+                  }
+              },
+              {
+                  "id": "P4",
+                  "data": {
+                      "first name": "Salma",
+                      "last name": "Alazem",
+                      "desc": "Mother of Reyad Alazazem",
+                      "label": "Salma Alazem",
+                      "gender": "F",
+                      "birthYear": 1953
+                  },
+                  "rels": {
+                      "spouses": ["P3"],
+                      "children": ["P1"]
+                  }
+              },
+              {
+                "id": "P5",
+                "data": {
+                    "first name": "Mohammad Ali",
+                    "last name": "Keylani",
+                    "desc": "Father of Salma Alazem",
+                    "label": "Mohammad Ali",
+                    "gender": "M",
+                    "birthYear": 1951
+                },
+                "rels": {
+                    "spouses": ["P6"],
+                    "children": ["P2"]
+                }
+            },
+              {
+                  "id": "P6",
+                  "data": {
+                      "first name": "Fatima",
+                      "last name": "Keylani",
+                      "desc": "Mother of Salma Alazem",
+                      "label": "Fatima Keylani",
+                      "gender": "F",
+                      "birthYear": 1952
+                  },
+                  "rels": {
+                      "spouses": ["P5"],
+                      "children": ["P2"]
+                  }
+              },
+           
+          ];
+      }
+      ;
+      
+
+      create(data());
+    };
+
+    // Initialize chart when component mounts or route changes
+    initializeChart();
+
+    // Add event listener for route changes
+    router.events.on('routeChangeComplete', initializeChart);
+
+    // Cleanup on unmount or before re-initialization
+    return () => {
+      cleanup();
+      router.events.off('routeChangeComplete', initializeChart);
+    };
+  }, [router]); // Re-run effect if router changes
+
+  return <div className="f3 f3-cont" id="FamilyChart" ref={containerRef}></div>;
 }
